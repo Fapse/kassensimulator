c63b1a731b91d2331897515fe953cfcbbe73ef7e
Fapse <fb-online@posteo.de>
2018-04-14 07:02:22 +0200

    JavaFX included

--- Kassensimulator/src/de/fapse/kassensimulator/Simulator.java
+++ Kassensimulator/src/de/fapse/kassensimulator/Simulator.java
@@ -7,22 +7,31 @@ import java.util.logging.Handler;
 import java.util.logging.Logger;
 
 import de.fapse.kassensimulator.model.SimModel;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Simulator {
+public class Simulator extends Application {
 
 	private static final Logger log = Logger.getLogger("de.fapse.kassensimulator");
 	private static Handler logFileHandler = null;
 	
 	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		try {
 			System.out.println("Test2");
 			logFileHandler = new FileHandler("kassensimulator.log");
 		} catch (IOException e){
 			
 		}
-	log.addHandler(logFileHandler);
-	// TODO Auto-generated constructor stub
+		log.addHandler(logFileHandler);
 		List<String> hsstellen = SimModel.getHsstellen();
 		hsstellen.stream().forEach(e -> System.out.println(e));
+		primaryStage.setTitle("Mein Kassensimulator");
+		primaryStage.show();
 	}
 }
