1703745c80241e3d26ee57a99be75fdaf19313c3
Fapse <fb-online@posteo.de>
2018-04-14 08:34:20 +0200

    Testclasses added

---
+++ Kassensimulator/test/de/fapse/kassensimulator/model/SimModelTest.java
@@ -0,0 +1,15 @@
+package de.fapse.kassensimulator.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimModelTest {
+	@Test
+	public void simpleTest() {
+		assertEquals("Schuhe","Schuhe");
+	}
+	
+
+
+}
